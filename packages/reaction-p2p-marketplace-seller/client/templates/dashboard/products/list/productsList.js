387adfb5d812c8a8b84edea056f3fdea2c9dfc3d
ScyDev <lukas.saegesser@gmail.com>
2016-06-09 13:05:23 +0200

    - wait for correct subscription - and do actually subscribe

--- packages/reaction-p2p-marketplace-seller/client/templates/dashboard/products/list/productsList.js
+++ packages/reaction-p2p-marketplace-seller/client/templates/dashboard/products/list/productsList.js
@@ -3,16 +3,21 @@ Template.dashboardProductsList.inheritsHelpersFrom("productList"); // for media
 Template.dashboardProductsList.inheritsHooksFrom("productList"); // needed to make products show up
 Template.dashboardProductsList.inheritsHelpersFrom("gridContent"); // for price
 
+Template.dashboardProductsList.onCreated(function() {
+  // Update product subscription
+  this.autorun(() => applyProductFilters());
+});
+
 Template.dashboardProductsList.helpers({
   products: function (data) { // override to show only this users products
     //let SellerProducts = Meteor.subscribe("SellerProducts");
-    if (ReactionCore.MeteorSubscriptions_SellerProducts.ready()) {
+    if (ReactionCore.MeteorSubscriptions_Products.ready()) {
       //console.log("helper Template.dashboardProductsList.helpers using publication SellerProducts.");
       return ReactionCore.Collections.Products.find({userId: Meteor.userId()});
     }
   },
   isProdsSubReady: function () {
-    if (ReactionCore.MeteorSubscriptions_SellerProducts.ready()) {
+    if (ReactionCore.MeteorSubscriptions_Products.ready()) {
       return true;
     }
     else {
